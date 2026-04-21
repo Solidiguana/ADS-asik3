d80eb688c298a72481422a0e732247c2e3df0389
Solidiguana <leonsolidiguana@gmail.com>
2026-04-21 17:06:18 +0500

    feat(sorter): added advanced sorting algorithm

--- src/Sorter.java
+++ src/Sorter.java
@@ -12,6 +12,38 @@ public class Sorter {
         }
     }
 
+    public void advancedSort(int[] arr) { //merge
+        if(arr.length <= 1) return;
 
+        int mid = arr.length / 2;
+        int[] left_half = new int[mid];
+        int[] right_half = new int[arr.length - mid];
 
+        System.arraycopy(arr, 0, left_half, 0, mid);
+        System.arraycopy(arr, mid, right_half, 0, arr.length - mid);
+
+        advancedSort(left_half);
+        advancedSort(right_half);
+
+        merge(arr, left_half, right_half);
+
+    }
+
+    public void merge(int[] arr, int[] left_half, int[] right_half){
+        int i = 0, j = 0, k = 0;
+        while(i < left_half.length && j < right_half.length){
+            if(left_half[i] <= right_half[j]){
+                arr[k++] = left_half[i++];
+            }
+            else{
+                arr[k++] = right_half[j++];
+            }
+        }
+        while(i < left_half.length){
+            arr[k++] = left_half[i++];
+        }
+        while(j < right_half.length){
+            arr[k++] = right_half[j++];
+        }
+    }
 }
